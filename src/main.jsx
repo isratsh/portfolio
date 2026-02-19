f67005e922bf45e72266702d20d82d436f9ab707
Israt Jahan Shikha <isratss588@gmail.com>
2026-02-19 12:43:33 +0600

    Final working version

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,9 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
+
